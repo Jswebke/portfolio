9f60ba244b8053b86deabc83e01ba748a399f811
shawn owiro <shawnowiro@gmail.com>
2023-11-27 20:28:20 +0300

    removed error when merging dev branch and about-us branch

--- frontend/jsweb/app/page.js
+++ frontend/jsweb/app/page.js
@@ -1,12 +1,5 @@
 'use client'
 import React from 'react'
-
-const index = () => {
-  return (
-    <div>
-      <a href='/about-us'>about us</a>
-    </div>
-
 import Image from 'next/image'
 import Link from 'next/link'
 import NavBar from './components/NavBar'
