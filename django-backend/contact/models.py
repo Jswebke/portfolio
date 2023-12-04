24046882646da592f369921db807e08975074d23
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-12-04 03:11:58 +0300

    contact page with django rest

---
+++ django-backend/contact/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
